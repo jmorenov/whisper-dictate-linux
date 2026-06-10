9b037fa979c6d578e2aa477720045533f1118ef2
jmorenov <jmorenov28@gmail.com>
2026-06-10 10:09:53 +0200

    Fix error of import

--- whisper_dictate/__init__.py
+++ whisper_dictate/__init__.py
@@ -4,7 +4,7 @@ __version__ = "1.0.0"
 
 from .audio import start_recording, stop_recording
 from .keyboard import find_keyboards
-from .config import load_config, DEFAULTS, CONFIG_PATH
+from .config import load_config, resolve_model, DEFAULTS, CONFIG_PATH
 from .whisper import transcribe
 from .notify import notify
 from .inject import inject
